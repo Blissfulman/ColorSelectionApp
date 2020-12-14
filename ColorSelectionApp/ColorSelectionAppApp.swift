3a5d12bf0ef46c99c06ba6478ab5236fb8572ab3
Evgeny Novgorodov <greatly01@ya.ru>
2020-12-14 21:08:47 +0500

    Initial Commit

---
+++ ColorSelectionApp/ColorSelectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSelectionAppApp.swift
+//  ColorSelectionApp
+//
+//  Created by User on 14.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ColorSelectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
